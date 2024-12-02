2c215b2bc56df19bf50f90df652320516d8c16a2
yhieuniehra <hra101237@donga.edu.vn>
2024-12-03 00:31:58 +0700

    admin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => console.error('MongoDB connection error: ', err));
 
 // Routes
-app.use('/admin', adminRoutes);  // Sử dụng /admin cho các route admin
+app.use('/', adminRoutes);  // Sử dụng /admin cho các route admin
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
